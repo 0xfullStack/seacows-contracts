bb4a9152753776d2b240d094542bbef8beb7af9b
0xknon <0xknon@gmail.com>
2023-05-04 14:00:43 +0800

    fix: update deployed location

--- packages/seacows-amm/tasks/utils/index.ts
+++ packages/seacows-amm/tasks/utils/index.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { type Environment } from '@yolominds/constants';
 
 const save = async (env: Environment, network: string, name: string, address: string): Promise<void> => {
-  const targetPath = path.join(__dirname, '../../../../deployed', env.toString(), network.toString(), name + '.json');
+  const targetPath = path.join(__dirname, '../../deployed', env.toString(), network.toString(), name + '.json');
   ensureDirectory(targetPath);
   fs.writeFileSync(targetPath, JSON.stringify({ address }, null, 2));
   console.log(`${name} of address ${address} deployment saved to ${targetPath}`);
