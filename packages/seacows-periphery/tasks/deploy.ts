76ece965a5a56570d438091bd1469d5f4c2bffcf
0xknon <0xknon@gmail.com>
2023-06-26 15:04:12 +0800

    feat: add deployed router

--- packages/seacows-periphery/tasks/deploy.ts
+++ packages/seacows-periphery/tasks/deploy.ts
@@ -3,7 +3,7 @@ import { addresses, type SupportedChain, type Environment } from '@yolominds/sea
 // import { addresses } from '../deployed';
 import { save } from './utils';
 
-export const deploy: ActionType<{ env: Environment }> = async ({ env }, { ethers, network }) => {
+export const deploy: ActionType<{ env: Environment }> = async ({ env }, { ethers, network, run }) => {
   const chainId = network.config.chainId as SupportedChain;
   const { weth } = addresses[env][chainId];
 
@@ -12,6 +12,11 @@ export const deploy: ActionType<{ env: Environment }> = async ({ env }, { ethers
     const router = await SeacowsRouterFC.deploy(weth);
     await router.deployed();
     await save(env, network.name, 'SeacowsRouter', router.address);
+
+    await run('verify:verify', {
+      address: router.address,
+      constructorArguments: [weth],
+    });
   } catch (error) {
     console.error('Error meesage:', error.message);
   }
