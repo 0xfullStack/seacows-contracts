f9d411670e8617d48ed562caf6ca33c8947d53cf
Lin <linshizai.me@gmail.com>
2023-11-16 10:44:43 +0800

    feat: remove mumbai network, add sepolia network

--- packages/seacows-sdk/deployed/dev/mumbai/index.ts
+++
@@ -1,9 +0,0 @@
-import { address as template } from './SeacowsERC721TradePair.json';
-import { address as manager } from './SeacowsPositionManager.json';
-
-export default {
-  weth: '0x6b6e69d2e2DE49e56Be57EADc750DD36D25c50f8',
-  royaltyRegistry: '0xb8b5eC5Fa0E20dED9dC86D029Ef24FdccE900a51',
-  template,
-  manager,
-};
