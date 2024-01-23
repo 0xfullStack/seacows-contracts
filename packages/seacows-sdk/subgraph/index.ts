51a3eab4977176d99c5cd7ca1741d38aeba024df
0xEddie <linshizai.me@gmail.com>
2024-01-23 15:10:20 +0800

    feat: subgraph metadata in sdk (#30)

---
+++ packages/seacows-sdk/subgraph/index.ts
@@ -0,0 +1,13 @@
+export enum SubgraphEndpoint {
+  MAINNET = 'https://subgraph-mainnet-prod.seacows.io/subgraphs/name/seacows/seacows-amm-subgraph',
+  GÖRLI = 'https://api.studio.thegraph.com/query/54972/goerli-seacows-amm/version/latest',
+  SEPOLIA = 'https://subgraph-sepolia-dev.seacows.io/subgraphs/name/seacows/seacows-amm-subgraph',
+  POLYGON = 'https://subgraph-polygon-prod.seacows.io/subgraphs/name/seacows/seacows-amm-subgraph',
+  MUMBAI = 'https://subgraph-mumbai-dev.seacows.io/subgraphs/name/seacows/seacows-amm-subgraph',
+}
+
+export enum SubgraphStudioEndpoint {
+  GÖRLI = 'https://api.studio.thegraph.com/query/54972/goerli-seacows-amm/version/latest',
+  SEPOLIA = 'https://api.studio.thegraph.com/query/54972/sepolia-seacows-amm/version/latest',
+  MUMBAI = 'https://api.studio.thegraph.com/query/54972/mumbai-seacows-amm/version/latest',
+}
